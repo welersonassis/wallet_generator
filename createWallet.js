f7ae2478b10824f8e4c6ee1dad1de2d296f58804
welerson <welerson_assis@hotmail.com>
2024-07-30 11:46:10 -0300

    Initial commit

---
+++ createWallet.js
@@ -0,0 +1,30 @@
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+const network = bitcoin.networks.testnet
+
+// wallet derivation 
+const path = `m/49'/1'/0'/0`
+
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+// wallet root
+let root = bip32.fromSeed(seed, network)
+
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh(
+    {
+        pubkey: node.publicKey,
+        network: network,
+    }
+).address
+
+console.log("Wallet generated")
+console.log("Address: ", btcAddress)
+console.log("Private key: ", node.toWIF())
+console.log("Seed", mnemonic)
+
